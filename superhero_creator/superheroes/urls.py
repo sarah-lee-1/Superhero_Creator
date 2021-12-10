0d82962f54ada52186b82d926065064b312ed9e2
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-12-10 15:35:37 -0600

    Populate and code links for Create, Index and Details files.

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
--- superhero_creator/superheroes/views.py
+++ superhero_creator/superheroes/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse 
 from .models import Superhero
 
 # Create your views here.
 def index(request):
+    # query set
     all_heroes = Superhero.objects.all()
     context = {
         'all_heroes': all_heroes
     }
     return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    # instance of the superhero class
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheroes/detail.html', context)
+    
+def create(request):
+    if request.method == "POST":
+        # save the form contents as a new db object
+        # return to index
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary_ability')
+        secondary = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+    return render(request, 'superheroes/create.html')
