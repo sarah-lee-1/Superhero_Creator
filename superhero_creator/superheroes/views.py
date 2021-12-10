1230fb52f02fd2cb85ca9d8aa35155f004076e69
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-12-10 10:54:12 -0600

    Add python interpreter, folders, subfolders and imports.

---
+++ superhero_creator/superheroes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Superhero
+
+# Create your views here.
+def index(request):
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
