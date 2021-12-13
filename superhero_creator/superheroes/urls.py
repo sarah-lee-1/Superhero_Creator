0fdbdb6a266bf8547090f926f9224f8a265bb70a
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-12-12 22:12:33 -0600

    Add edit url code to urls file

--- superhero_creator/superheroes/urls.py
+++ superhero_creator/superheroes/urls.py
@@ -6,6 +6,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create') 
-    path('delete/', views.delete, name='delete') 
+    path('new/', views.create, name='create'), 
+    path('delete/', views.delete, name='delete'), 
+    path('edit/', views.edit, name='edit'), 
 ]
